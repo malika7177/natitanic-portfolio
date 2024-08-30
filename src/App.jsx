f8390a5b68c815f563204a99fdf381a11d6771a8
malika7177 <malikadjrv@gmail.com>
2024-08-30 10:06:21 +0500

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home'
+import Testimonials from './components/Testimonials/Testimonials'
+import About from './components/About/About'
+import Portfolio from './components/Portfolio/Portfolio'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
     <div>
-      App
+      <Navbar />
+      <Home />
+      <Testimonials />
+      <About />
+      <Portfolio />
+      <Footer />
     </div>
   )
 }
