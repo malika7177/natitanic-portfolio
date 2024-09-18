6c64d6c22eb3a4d7f50e6442cad0be776c00bf31
malika7177 <malikadjrv@gmail.com>
2024-09-18 19:54:21 +0500

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import './home.css'
+import hero from '../../assets/Gleb.png'
 
 const Home = () => {
   return (
-    <div>
-      home
-    </div>
+    <section id='home' className='container'>
+      <div className="home-intro">
+        <h2>Hello</h2>
+        <h1>I'm Gleb Kostrubov</h1>
+        <p>I've been doing web design, front-end and back-end development for a year now. Do you need a website design, site layout, or maybe a turnkey website? Then contact me</p>
+        <a href='#contact'>Contact me</a>
+      </div>
+      <div className="home-img">
+        <img src={hero} alt="" />
+      </div>
+    </section>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,43 +1,43 @@
-import React, { useState } from "react";
-import "./navbar.css";
-import logo from "../../assets/logo.png";
+  import React, { useState } from "react";
+  import "./navbar.css";
+  import logo from "../../assets/logo.png";
 
-const Navbar = () => {
-  const [activeLink, setActiveLink] = useState(null);
-  const links = [
-    { text: "Home", href: "#home" },
-    { text: "About", href: "#about" },
-    { text: "Portfolio", href: "#portfolio" },
-    { text: "Contact", href: "#contact" },
-  ];
+  const Navbar = () => {
+    const [activeLink, setActiveLink] = useState(null);
+    const links = [
+      { text: "Home", href: "#home" },
+      { text: "About", href: "#about" },
+      { text: "Portfolio", href: "#portfolio" },
+      { text: "Contact", href: "#contact" },
+    ];
 
-  const handleClick = (event) => {
-    setActiveLink(event.target.textContent);
+    const handleClick = (event) => {
+      setActiveLink(event.target.textContent);
+    };
+
+    return (
+      <header className="container">
+        <nav>
+          <div className="logo-container">
+            <img src={logo} alt="Logo" />
+            <a href="#" className="logo">Notitanic</a>
+          </div>
+          <ul className="nav-links">
+            {links.map((link) => (
+              <li key={link.text}>
+                <a
+                  href={link.href}
+                  className={activeLink === link.text ? "active" : ""}
+                  onClick={handleClick}
+                >
+                  {link.text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+    );
   };
 
-  return (
-    <header className="container">
-      <nav>
-        <div className="logo-container">
-          <img src={logo} alt="Logo" />
-          <a href="#" className="logo">Notitanic</a>
-        </div>
-        <ul className="nav-links">
-          {links.map((link) => (
-            <li key={link.text}>
-              <a
-                href={link.href}
-                className={activeLink === link.text ? "active" : ""}
-                onClick={handleClick}
-              >
-                {link.text}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  );
-};
-
-export default Navbar;
+  export default Navbar;
